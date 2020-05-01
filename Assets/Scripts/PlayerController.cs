c03be19fbe91ab2db3b40a9cee56c5c49d56b210
Bulldison <62094411+Bulldison@users.noreply.github.com>
2020-05-01 13:36:04 +0200

    PPM
    Création d'une plate-forme de test
    
    Gestion de la physique de la plate-forme + du personnage
    
    Mise en place des mouvements du personnage :
    
    - Le personnage peut marcher à droite et à gauche
    
    - Le personnage peut sauter ( pas de saut infini / on pourra peut êtres lui autoriser des doubles sauts )
    
    Gestion de la camera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+
+	
+{
+		public float moveSpeed;
+		public Rigidbody2D theRB;
+		public float jumpForce;
+		
+		public bool isGrounded;
+		public Transform groundCheckPoint;
+		public LayerMask whatIsGround;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
+		
+		isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+		
+		if(Input.GetButtonDown("Jump"))
+		{
+			if (isGrounded)
+			{
+				theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+			}
+		}
+    }
+}
