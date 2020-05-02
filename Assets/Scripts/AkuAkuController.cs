1171bb48d7cc6d17ce6a05a7b341fe3d090ea906
xTiiX <arthur.nordmann@supinfo.com>
2020-05-02 17:08:15 +0200

    Aku's movement

--- Assets/Scripts/AkuAkuController.cs
+++ Assets/Scripts/AkuAkuController.cs
@@ -2,14 +2,20 @@
 
 public class AkuAkuController : MonoBehaviour
 {
-    public Transform target;
+    public GameObject target;
 
-    public float smoothSpeed = 5f;
+    public float smoothSpeed = 15f;
     public Vector3 offset;
 
     private void LateUpdate()
     {
-        Vector3 finalPos = target.position + offset;
+        //Player's direction
+        if (target.GetComponent<PlayerController>().theSR.flipX)
+        {
+            offset = new Vector3(-offset.x, offset.y, offset.z);
+        }
+
+        Vector3 finalPos = target.transform.position + offset;
         Vector3 smoothPos = Vector3.Lerp(transform.position, finalPos, smoothSpeed * Time.deltaTime);
 
         transform.position = smoothPos;
