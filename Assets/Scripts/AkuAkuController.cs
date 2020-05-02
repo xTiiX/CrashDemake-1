9551edd79e1d49650f7c14c6f7a3cd5687f83129
xTiiX <arthur.nordmann@supinfo.com>
2020-05-02 14:50:25 +0200

    AkuAkuController

---
+++ Assets/Scripts/AkuAkuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AkuAkuController : MonoBehaviour
+{
+    public Transform crash;
+
+    public float smoothSpeed = 5f;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+
+        Vector3 finalPos = crash.position + offset;
+        Vector3 smoothPos = Vector3.Lerp(transform.position, finalPos, smoothSpeed * Time.deltaTime);
+
+        transform.position = smoothPos;
+    }
+}
