29cb7947d9ddb61ac25c7c67e8de2053eb5b8daf
xTiiX <arthur.nordmann@supinfo.com>
2020-05-02 17:19:19 +0200

    Aku's movement final

--- Assets/Scripts/AkuAkuController.cs
+++ Assets/Scripts/AkuAkuController.cs
@@ -7,12 +7,22 @@ public class AkuAkuController : MonoBehaviour
     public float smoothSpeed = 15f;
     public Vector3 offset;
 
+    private int left = 2;
+    private int right = -2;
+
     private void LateUpdate()
     {
         //Player's direction
         if (target.GetComponent<PlayerController>().theSR.flipX)
         {
-            offset = new Vector3(-offset.x, offset.y, offset.z);
+            offset = new Vector3(left, offset.y, offset.z);
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+        else
+        {
+            offset = new Vector3(right, offset.y, offset.z);
+            transform.rotation = new Quaternion(0,180,0,0);
+
         }
 
         Vector3 finalPos = target.transform.position + offset;
