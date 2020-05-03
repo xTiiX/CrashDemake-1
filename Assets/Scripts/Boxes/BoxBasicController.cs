bcae465f5e65c81430579e03d9ce4d373438366a
noahGms <noah@keshin.net>
2020-05-03 17:23:25 +0200

    box basic controller

--- Assets/Scripts/Boxes/BoxBasicController.cs
+++ Assets/Scripts/Boxes/BoxBasicController.cs
@@ -1,14 +1,30 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class BoxBasicController : MonoBehaviour
 {
+    public static BoxBasicController instance;
+
     public GameObject crash;
     public GameObject wumpa;
 
+    private void Awake()
+    {
+        instance = this;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        BoxDestroy(collision);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        BoxDestroy(collision);
+    }
+
+    public void BoxDestroy(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
         {
             if (crash.GetComponent<PlayerController>().isInAttack)
             {
