c5fd2e737d82adbf456bc88f0a2774a58cd578dc
Bulldison <62094411+Bulldison@users.noreply.github.com>
2020-05-03 18:08:36 +0200

    PPM7

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -39,49 +39,53 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
-
-		if (knockBackCounter <= 0)
-		{
-			theRB.velocity = new Vector2(horizontal * moveSpeed, theRB.velocity.y);
-			
-			isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
-			
-			if (Input.GetButtonDown("Jump"))
-			{
-				if (isGrounded)
-				{
-					theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
-				}
-			}
-
-            if (Input.GetButtonDown("Attack"))
-            {
-                StartCoroutine(crashAttack());
-            }
-			
-			if (theRB.velocity.x < 0)
-			{
-				theSR.flipX = true;
-			} else if(theRB.velocity.x > 0)
-			{
-				theSR.flipX = false;
-			}
 		
-		} else
+		if(!PauseMenu.instance.isPaused)
 		{
-			knockBackCounter -= Time.deltaTime;
-			if (!theSR.flipX)
+
+			if (knockBackCounter <= 0)
 			{
-				theRB.velocity = new Vector2(-knockBackForce, theRB.velocity.y);
+				theRB.velocity = new Vector2(horizontal * moveSpeed, theRB.velocity.y);
+				
+				isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+				
+				if (Input.GetButtonDown("Jump"))
+				{
+					if (isGrounded)
+					{
+						theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+					}
+				}
+
+				if (Input.GetButtonDown("Attack"))
+				{
+					StartCoroutine(crashAttack());
+				}
+				
+				if (theRB.velocity.x < 0)
+				{
+					theSR.flipX = true;
+				} else if(theRB.velocity.x > 0)
+				{
+					theSR.flipX = false;
+				}
+			
 			} else
 			{
-				theRB.velocity = new Vector2(knockBackForce, theRB.velocity.y);
+				knockBackCounter -= Time.deltaTime;
+				if (!theSR.flipX)
+				{
+					theRB.velocity = new Vector2(-knockBackForce, theRB.velocity.y);
+				} else
+				{
+					theRB.velocity = new Vector2(knockBackForce, theRB.velocity.y);
+				}
 			}
+			
+			anim.SetFloat("moveSpeed", Mathf.Abs(theRB.velocity.x));
+			anim.SetBool("isGrounded", isGrounded);
+			anim.SetBool("isInAttack", isInAttack);
 		}
-		
-		anim.SetFloat("moveSpeed", Mathf.Abs(theRB.velocity.x));
-		anim.SetBool("isGrounded", isGrounded);
-        anim.SetBool("isInAttack", isInAttack);
     }
 	
 	public void knockBack()
