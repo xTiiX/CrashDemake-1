251536ba6d6a599a0c3737e1aa58a5fbdcefcd3a
noahGms <noah@keshin.net>
2020-05-03 14:02:09 +0200

    some updates

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
@@ -20,7 +18,6 @@ public class UIController : MonoBehaviour
         UI.GetComponent<Text>().text = PlayerHealthController.instance.maxHealth + "";
     }
 
-    // Update is called once per frame
     void Update()
     {
         
