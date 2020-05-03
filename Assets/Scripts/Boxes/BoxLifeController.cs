c917aba81c050b73dd5f4ccb89564525d407c3b5
xTiiX <arthur.nordmann@supinfo.com>
2020-05-03 17:41:17 +0200

    BoxLife + AnimCrash Done

---
+++ Assets/Scripts/Boxes/BoxLifeController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class BoxLifeController : MonoBehaviour
+{
+    public static BoxLifeController instance;
+
+    public GameObject crash;
+    public GameObject life;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        BoxDestroy(collision);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        BoxDestroy(collision);
+    }
+
+    public void BoxDestroy(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (crash.GetComponent<PlayerController>().isInAttack)
+            {
+                Destroy(gameObject);
+                life.SetActive(true);
+            }
+        }
+    }
+}
