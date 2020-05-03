556bac36f2b0bc6880ce6350c6949281bddf580e
xTiiX <arthur.nordmann@supinfo.com>
2020-05-03 17:58:52 +0200

    AkuAkuBox

---
+++ Assets/Scripts/Boxes/BoxAkuAkuController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class BoxAkuAkuController : MonoBehaviour
+{
+    public static BoxAkuAkuController instance;
+
+    public GameObject crash;
+    public GameObject akuAku;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        BoxDestroy(collision);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        BoxDestroy(collision);
+    }
+
+    public void BoxDestroy(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (crash.GetComponent<PlayerController>().isInAttack)
+            {
+                Destroy(gameObject);
+                akuAku.SetActive(true);
+            }
+        }
+    }
+}
