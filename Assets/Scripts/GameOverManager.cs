d5dd404317c7e83d7d7412f9c33b76675efefb35
Bulldison <62094411+Bulldison@users.noreply.github.com>
2020-05-04 12:55:24 +0200

    PPM9

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOverManager : MonoBehaviour
+{
+	public string gameoverScreen;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerHealthController.instance.currentHealth == 0)
+		{
+			SceneManager.LoadScene(gameoverScreen);
+		} 
+    }
+}
---
+++ Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+	public string restartScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	public void RestartGame()
+	{
+		SceneManager.LoadScene(restartScene);
+	}
+}
